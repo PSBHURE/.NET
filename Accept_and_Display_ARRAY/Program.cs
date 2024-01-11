8949d9ed3adfa87497906e6d93a966d3f82beeb9
pratik <bhurepratik2@gmail.com>
2024-01-11 23:17:11 +0530

    accept and display element for array

---
+++ Accept_and_Display_ARRAY/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ARRAY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 0;
+            Console.WriteLine("enter size of array");
+            size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            //Console.WriteLine(size);
+            for(int i = 0; i < size;i++)
+            {
+                Console.WriteLine($"enter {i+1} element in array:");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0;i < size;i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
