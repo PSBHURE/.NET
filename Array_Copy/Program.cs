6b5a53ab975711c6c09ef2ab9cb5bb39e92adcbe
pratik <bhurepratik2@gmail.com>
2024-01-11 23:30:23 +0530

    copy one array to another array

---
+++ Array_Copy/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Array_Copy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 0;
+            Console.WriteLine("enter size of array");
+            size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"enter {i + 1} element in array:");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+        public void ArrayCopy(int[] arr)
+        {
+            int[] arr1 = new int[arr.Length];
+            for(int i = 0; i<arr.Length; i++)
+            {
+                arr1[i] = arr[i];
+            }
+            Console.WriteLine("element of 2nd array");
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.Write(arr1[i] + " ");
+            }
+        }
+    }
+}
