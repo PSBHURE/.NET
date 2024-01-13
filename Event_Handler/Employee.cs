9cdd6ba0c6d9e42975b0b58e48aa1821941d6371
pratik <bhurepratik2@gmail.com>
2024-01-13 12:19:44 +0530

    1st try for event Handling

---
+++ Event_Handler/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Handler
+{
+    public delegate void InvalidInput();
+    internal class Employee
+    {
+        public event InvalidInput InvalidateBasic;
+        private int basic;
+        public int Basic
+        {
+            set
+            {
+                if (value > 10000)
+                    this.basic = value;
+                else
+                    if (InvalidateBasic != null)
+                    InvalidateBasic();
+            }
+            get { return this.basic; }
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("Hello IACSD");
+        }
+    }
+}
---
+++ Event_Handler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Event_Handler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.InvalidateBasic += InvalidateBasic;
+            e1.Basic = 1000;
+        }
+        static void InvalidateBasic()
+        {
+            Console.WriteLine("my event handling code called");
+        }
+    }
+}
